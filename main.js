b67024ed8f189514b211b11fee332bd3a3d6ba71
Valentin Catalin Bengea <valentinbengea@gmail.com>
2024-05-27 22:41:57 +0200

    Add functions for basic math operators

--- main.js
+++ main.js
@@ -4,13 +4,34 @@ function add(a,b) {
     return a + b;
 };
 
+function substract(a,b) {
+    return a - b;
+};
+
+function division(a,b) {
+    if (b === 0) {
+        return "Not today";
+    } else {
+        return (a / b).toFixed(3);
+    };
+};
+
+function multiply(a,b) {
+    return a * b;
+};
+
 
 const test = document.querySelector(".buttons");
 const display = document.querySelector(".display");
 
 test.addEventListener("click", (e) => {
-    let tester = document.createElement("div");
-    tester.classList.add("tester")
-    tester.textContent = "hola";
-    display.appendChild(tester);
-})
+    if (e.target.textContent === "AC") {
+        display.textContent = "";
+    } else {
+        let tester = document.createElement("div");
+        tester.classList.add("tester");
+        tester.textContent = e.target.textContent;
+        display.appendChild(tester);
+    }
+    
+});
