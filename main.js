65700cf4889ea44c2d0f0a5f9461ee81fd95e7df
Valentin Catalin Bengea <valentinbengea@gmail.com>
2024-05-29 22:59:44 +0200

    Minor change in index.html and main.js

--- main.js
+++ main.js
@@ -1,26 +1,4 @@
 
-
-//function add(a,b) {
-//    return a + b;
-//};
-//
-//function substract(a,b) {
-//    return a - b;
-//};
-//
-//function division(a,b) {
-//    if (b === 0) {
-//        return "Not today";
-//    } else {
-//        return (a / b).toFixed(3);
-//    };
-//};
-//
-//function multiply(a,b) {
-//    return a * b;
-//};
-
-//Using switch to return an operation based on the operator.
 function operate(a,b,operator) {
     switch(operator) {
         case("+"):
@@ -42,18 +20,67 @@ function operate(a,b,operator) {
 const test = document.querySelector(".buttons");
 const display = document.querySelector(".display");
 
-let arrayOfElements = [];
+let arrayOfFirst = [];
+let arrayOfSecond = [];
+let arrayOfOperator = [];
+
+// Maybe an object instead of arrays;
 
 
 test.addEventListener("click", (e) => {
     if (e.target.textContent === "AC") {
         display.textContent = "";
-    } else {
-        let tester = document.createElement("div");
-        tester.classList.add("tester");
-        tester.textContent = e.target.textContent;
-        arrayOfElements.push(Number(tester.textContent));
-        display.appendChild(tester);
+        arrayOfFirst = [];
+        arrayOfSecond = [];
+        arrayOfOperator = [];
+    }
+
+    else if (arrayOfOperator.length === 0) {
+        // To Do
+        // Nest another check to see if arrayOfFirst lenght is 0;
+        let value = e.target.classList;
+        if (value.value === "operator") {
+            console.log("This works");
+            arrayOfOperator.push(e.target.textContent);
+        }
+        else {
+            arrayOfFirst.push(Number(e.target.textContent));
+        }
+    }
+    else if (arrayOfOperator.length === 1) {
+        let value = e.target.classList;
+        if (value.value === "operator") {
+            //To do;
+            console.log("//To Do")
+        }
+        else {
+            arrayOfSecond.push(Number(e.target.textContent));
+        }
+    }
+    else if (arrayOfOperator.length === 2) {
+        console.log("Deal with second operator")
+        // return first operation;
+        // store second operator in a variable
+        // start again with fresh variable.
     }
     
 });
+
+
+// When AC it's clicked, clear all. (display and variables)
+// While operator lenght = 0, store numbers into first variable
+// When operator lenght = 1, store numbers into second variable
+// When operator lenght = 2, return the result of the first operation, and store the second operator to continue calculus.
+
+
+
+
+// First, we store the numbers in the first value
+// When an operator it's clicked, store operator and move to the second value
+// Store the numbers in the second value
+// If an operator it's clicked again {
+    // Operate with the first two values and return the result
+    // Store the result in the first variable 
+    // Update the operator with the new one
+    // Add the new numbers into the second value
+//}
